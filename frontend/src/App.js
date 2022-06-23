bd94208c554cd418f74f006e34c54407008f4e62
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-06-23 16:54:06 -0300

    initial frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//pages and components
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <div className="App">
+			<BrowserRouter>
+				<Navbar />
+				<div className="pages">
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+    </div>
+  );
+}
