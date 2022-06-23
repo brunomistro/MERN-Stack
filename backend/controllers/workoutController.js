43c727b67238387daf6e9645500c530d8d88deb0
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-06-22 22:38:03 -0300

    CRUD done

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -40,12 +40,36 @@ const createWorkout = async (req, res) => {
 
 // delete item
 const deleteWorkout = async (req, res) => {
-	
+	const { id } = req.params;
+
+ if(!mongoose.Types.ObjectId.isValid(id)) {
+	return res.status(404).json({error: "Invalid ID"})
+ }
+
+ const workout = await Workout.findOneAndDelete({_id: id})
+
+ if(!workout) {
+	return res.status(404).json({error: "Not Found"})
+ }
+
+ res.status(200).json(workout)
 }
 
 // update item
 const updateWorkout = async (req, res) => {
-	
+	const { id } = req.params;
+
+ if(!mongoose.Types.ObjectId.isValid(id)) {
+	return res.status(404).json({error: "Invalid ID"})
+ }
+
+ const workout = await Workout.findOneAndUpdate({_id: id}, { ...req.body })
+
+ if(!workout) {
+	return res.status(404).json({error: "Not Found"})
+ }
+
+ res.status(200).json(workout)
 }
 
 module.exports = {
