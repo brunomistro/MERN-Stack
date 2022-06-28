f86fd459f0667c3eae4072bfa42eec2e33aba1ad
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-06-28 16:11:22 -0300

    post item

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 export default function Home() {
 	const [workouts, setWorkouts] = useState(null)
@@ -20,10 +21,11 @@ export default function Home() {
 	return (
 		<div className="home">
 			<div className="workouts">
-				{workouts && workouts.map((workout) => (
+				{workouts && workouts.map(workout => (
 					<WorkoutDetails workout={workout} key={workout._id} />
 				))}
 			</div>
+			<WorkoutForm />
 		</div>
 	)
 }
